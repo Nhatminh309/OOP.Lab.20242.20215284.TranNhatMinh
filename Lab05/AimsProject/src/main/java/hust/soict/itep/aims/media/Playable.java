4fc60b41d6f63b3048938043355505d3263d0f52
Nhatminh309 <nhatminh2k22k3@gmail.com>
2025-05-31 18:19:02 +0700

    update final

--- Lab05/AimsProject/src/main/java/hust/soict/itep/aims/media/Playable.java
+++
@@ -1,8 +0,0 @@
-package hust.soict.itep.aims.media;
-
-import hust.soict.itep.aims.exception.PlayerException;
-
-public interface Playable {
-    // Method to play
-    public void play() throws PlayerException;
-}
